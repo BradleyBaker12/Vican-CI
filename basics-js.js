977af4804e4a75fa62a4d230e2d68a5360d8c1ec
BradleyBaker12 <141717112+BradleyBaker12@users.noreply.github.com>
2023-08-10 22:23:13 +0200

    Add files via upload

---
+++ basics-js.js
@@ -0,0 +1,27 @@
+// Get the square and line elements
+const square = document.querySelector('.square');
+const line = document.querySelector('.line');
+const body = document.body;
+
+
+// Listen for the scroll event
+window.addEventListener('scroll', () => {
+    // Calculate the scroll progress
+    const scrollPercentage = (window.scrollY / (document.documentElement.scrollHeight - window.innerHeight)) * 100;
+
+    // Calculate the new position for the square
+    const maxPosition = line.clientHeight - square.clientHeight;
+    const newPosition = (maxPosition * scrollPercentage) / 100;
+
+    // Update the square's position
+    square.style.transform = `translateY(${newPosition}px)`;
+});
+
+document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+    anchor.addEventListener('click', function (e) {
+        e.preventDefault();
+        document.querySelector(this.getAttribute('href')).scrollIntoView({
+            behavior: 'smooth'
+        });
+    });
+});
